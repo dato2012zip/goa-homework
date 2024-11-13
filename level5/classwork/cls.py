09ea5c586d47e3ffa88284f9af66c87f1098373f
Hello <ddevadze12@gmail.com>
2024-11-13 14:23:30 +0400

    second commit

---
+++ level5/classwork/cls.py
@@ -0,0 +1,45 @@
+# 1
+greeting = "Hello"
+userName = "student"
+age = 20
+location = "New York"
+
+#2
+print('Welcome, ' + userName + "!" ) 
+print(greeting + ", " + userName + "!") 
+
+
+
+#3
+favoriteNumber = 7
+print("Your favorite number doubled is " + str(favoriteNumber) * 2)  
+
+#4
+total_score = 100
+bonus_points = 10
+finalScore = total_score + bonus_points
+print("Your final score is " + str(finalScore))
+
+#5
+days_in_a_week = 7
+print("There are " + days_in_a_week * "4" + " days in four weeks")
+
+#6
+firstName = "John"
+lastname = "sins"
+print("Hello, " + firstName + " " + lastname + "!")
+
+#7
+nickname = "Ace"
+print("Your nickname is: " + nickname)
+
+
+
+#9
+year = 2024
+print("Next year is " + str(year + 1))
+print("Your age next year will be " + str(age + 1))
+
+#10
+farewell = "Goodbye"
+print(farewell, userName + "! See you next time.")
---
+++ level5/main.py
@@ -0,0 +1,11 @@
+# 1
+current_year = 2024
+print("The current year is " + str(current_year))
+
+# 2
+country = "USA"
+print("You are from " + country)
+
+# 3
+new_name = "Alex"
+print("Hello, " + new_name)
