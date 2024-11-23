6aa1c6e452617be8897b7caa66633142b99ad6e2
Hello <ddevadze12@gmail.com>
2024-11-23 10:43:47 +0400

    fourth

---
+++ level 07/classwork/main1.py
@@ -0,0 +1,7 @@
+print("C A L C U L A T O R")
+num1 = int(input("შეიყვანე პირველი რიცხვი: "))
+num2 = int(input("შეიყვანე მეორე რიცხვი: "))
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
---
+++ level 07/homework/hw2.py
@@ -0,0 +1,10 @@
+print("C A L C U L A T O R")
+# აქ ვწერ პირველ რიცხვს
+num1 = int(input("შეიყვანე პირველი რიცხვი: "))
+# აქ ვწერ მეორე რიცხვს
+num2 = int(input("შეიყვანე მეორე რიცხვი: "))
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+#აქ ვპრინტავ
