da4832600aa045e9ccff96e9c9281094f9f3b129
Hello <ddevadze12@gmail.com>
2025-01-28 22:20:50 +0400

    commit

---
+++ level 16/classwork2/da.py
@@ -0,0 +1,11 @@
+number = int(input("შეიყვანეთ რიცხვი: "))
+result = number * 5
+print("რიცხვი გამრავლებული 5-ზე: ", result)
+
+
+
+    
+my_variable = 15
+print("დასაწყისი:", my_variable)
+my_variable = 25
+print("ახალი მნიშვნელობა:", my_variable)
