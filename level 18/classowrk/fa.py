642040289916e9ac4e27a1cabd184e2ab0e4a04d
Hello <ddevadze12@gmail.com>
2025-02-23 15:29:39 +0400

    commit

---
+++ level 18/classowrk/fa.py
@@ -0,0 +1,10 @@
+for i in range(20, 40):
+    print(i)
+
+name = input("შეიყვანეთ თქვენი სახელი: ")
+for i in range(15):
+    print(name)
+
+    
+for i in range(2, 21, 2):
+    print(i)
---
+++ level 18/homework/dfaf.py
@@ -0,0 +1,6 @@
+for i in range(100):
+    print("Goa")
+for i in range(20, 101, 5):
+  print(i)
+
+
