dd35d4c0ea905083cf6bdbe3b559dcb7a5716ccb
Hello <ddevadze12@gmail.com>
2025-03-27 21:12:07 +0400

    commit

---
+++ level 24/homework/gas.py
@@ -0,0 +1,25 @@
+# 1
+for i in range(101):
+    if i in [15, 27, 67, 32, 99, 93]:
+        continue
+    print(i)
+
+    #2
+
+count = 0
+
+while True:
+    count += 1
+    if count == 23:
+        break
+    print(count)
+
+ #3
+my_list = [1, 2, 3, 4, 5]
+
+my_list.append(6)
+my_list.append(7)
+
+print(my_list)
+
+
